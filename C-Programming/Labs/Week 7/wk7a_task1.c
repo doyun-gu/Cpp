5eeaeacf2affe16257ef62062affe621b1661994
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-10 02:05:09 +0100

    Create wk7a_task1.c

---
+++ C-Programming/Labs/Week 7/wk7a_task1.c
@@ -0,0 +1,40 @@
+/*
+Download the file Lab_7a_Task1 from Blackboard and complete the instructions given in the comments.
+*/
+
+/* Lab 7a Task 1  */
+
+/* Complete the six steps as described in the comments. Step 1 is given to you.
+
+*/
+
+#include <stdio.h>
+
+
+int main(void) {
+    /* Step 1: Declare a double variable d, and a double pointer variable d_ptr: */
+    double d, * d_ptr;
+
+    /* Step 2: Write a statement that makes d_ptr point to d: */
+
+
+
+    /* Complete the remaining steps by writing statements that use
+       only the pointer variable d_ptr (AND NOT using d): */
+
+       /* Step 3: Assign 3.0 to d (Remember to use only d_ptr in the statement!) */
+
+
+       /* Step 4: Write a single statement that prints out the value of d
+          (remembering to use only d_ptr in the statement) : */
+
+
+          /* Step 5: Write a single statement that prints out the address of d: */
+
+
+          /* Step 6: Write a single statement that prints out the value of d multiplied by 5
+             (remembering to use only d_ptr in the statement): */
+
+
+    return 0;
+}
