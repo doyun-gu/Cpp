4d2ee490a645a2031fb78c6ece76c92a3f5d8794
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-10 01:57:37 +0100

    Create wk4a_task3.c

---
+++ C-Programming/Labs/Week 4/wk4a_task3.c
@@ -0,0 +1,52 @@
+/*
+Task 3
+Download the file Lab_4a_Task3 from Blackboard and complete the instructionsbgiven in the comments
+*/
+
+/* Lab 4a Task 3 */
+
+#include <stdio.h>
+
+int main(void) {
+
+
+    /* Step a):
+       Try to predict the result of the following do loop: */
+       /*
+       int i = 9384;
+       do {
+          printf("%d ", i);
+          i /= 10;
+       } while (i>0);
+       */
+       /* Test you prediction by un-commenting the code, compiling and running it */
+
+
+    /* Step b)
+       Re-write the loop shown in Step a as a for loop.
+       <write your code below, and test the program>  */
+
+
+
+
+
+       /* Step c):
+          Try to predict the result of the following for loop: */
+          /*
+          int j;
+          for (i=5,j=i-1; i>0; --i,j=i-1)  {
+              printf("i = %d, j = %d\n",i, j);
+          }
+          */
+          /* Test you prediction by un-commenting the code, compiling and running it */
+
+
+
+       /* Step d)
+          Re-write the loop shown in Step c as a while loop.
+          <write your code below, and test the program>  */
+
+
+
+    return 0;
+}
