39ca664e5ad5bd95865506cdf8463f0306d4edba
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-10 01:32:21 +0100

    Create task2.c

---
+++ C-Programming/Labs/Week 1/task2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+
+	printf("Name \t Course \t Place of Birth\n");
+	printf("----------------------------------\n");
+	printf("<Doyun GU>\t <EEE>\t <16th April 2002>\n");
+	printf("<Sumin JIn>\t <Management>\t<25th January 2000>\n");
+
+	return 0;
+}
