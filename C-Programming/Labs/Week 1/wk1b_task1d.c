07bee387f6726481747d1b81f5215a7bb8cd9d2a
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-10 01:40:40 +0100

    Create wk1b_task1d.c

---
+++ C-Programming/Labs/Week 1/wk1b_task1d.c
@@ -0,0 +1,14 @@
+/*
+d) angular frequency = 2πf, where f=linear frequency.
+*/
+
+# include <stdio.h>
+int main(void) {
+	long omega, frequency=300;
+
+	omega = 2 * 3.14 * 30000;
+
+	printf("angular frequency is %d\n", omega);
+
+	return 0;
+}
