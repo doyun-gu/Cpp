dc1bb39c95e213869a8ff1dec638071faa8e204f
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-10 01:43:48 +0100

    Create wk2_task4.c

---
+++ C-Programming/Labs/Week 2/wk2_task4.c
@@ -0,0 +1,20 @@
+/*
+Quickly write a program to test whether the following list of names are legal identifiers, or not.
+
+List of names:
+1. times10
+2. 10times
+3. _done
+4. _10times
+5. user name (<-- this name has a space between "user" and "name")
+6. user.name (<-- this name has a "." between "user" and "name")
+7. user_name
+8. username
+
+   (hint: declare a separate variable for each name and see which ones compile successfully, or not)
+   
+    For example:
+	int times10;
+	int 10times;
+	…etc
+*/
