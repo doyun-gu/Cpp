5ec7e6cfed1776fc57a586004bd2dee642be7a70
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-16 04:37:31 +0900

    Create wk8a_task1.c

---
+++ C-Programming/Labs/Week 8/wk8a_task1.c
@@ -0,0 +1,27 @@
+/* Lab 8a Task1 Solution */
+
+/* In each the following steps you are asked to identify 
+   which of the following calls don't work and explain why.
+   
+   (a) printf("%c", '\n');      // correct
+   (b) printf("%c", "\n");      // incorrect: use "%c" only to write a char value
+   (c) printf("%s", '\n');      // incorrect: use "%s" only to write a string (a value of type char *)
+   (d) printf("%s", "\n");      // correct
+   (e) printf('\n');            // incorrect: printf requires a string, not a character
+   (f) printf("\n");            // correct
+   (g) putchar('\n');           // correct
+   (h) putchar("\n");           // incorrect: putchar requires a character, not a string
+   (i) puts('\n');              // incorrect: puts requires a string, not a character
+   (j) puts("\n");              // incorrect: this writes two new-line characters
+   (k) puts("");                // correct
+
+*/
+
+#include <stdio.h>
+
+int main(void)  {
+
+    /* write the code below and test */
+
+    return 0;
+}
