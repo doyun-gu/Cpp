cb28657fcffc0e638ee91094fea4653e9d3d24d5
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-16 04:57:03 +0900

    Create wk9b_task1.c

---
+++ C-Programming/Labs/Week 9/wk9b_task1.c
@@ -0,0 +1,51 @@
+/*
+Write a program that declares a union called ‘info’ to store the following, uninitialized, data:
+char c
+int integer
+unsigned int u_integer
+long long_int
+unsigned long u_long_int
+float f
+double d
+long double l_d
+
+Add a statement that prints out the size of the union, and make sure you can explain your result.
+*/
+
+/* Lab 9b Task1 Solution */
+
+/* Write a program that declares a union called 'info' to store the following, 
+   un-initialized, data:
+	char c
+	int integer
+	unsigned int u_integer
+	long long_int
+	unsigned long u_long_int
+	float f
+	double d
+	long double l_d
+
+Add a statement that prints out the size of the union, and explain your result.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  union {
+    char ch;
+    int integer;
+    unsigned int u_integer;
+    long long_int;
+    unsigned long u_long_int;
+    float f;
+    double d;
+    //long double l_d;
+  } info;
+  
+  printf ("The sizeof the union = %ld\n", (unsigned long) sizeof (info));
+  /* result is 16 bytes (128 bits) -the size of long double on a typical machine */
+
+
+  return 0;
+}
