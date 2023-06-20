bdfe31b2a2d02d2a3de7de8258a2d481792f4572
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-20 10:37:05 +0900

    Create 1 printf.c

---
+++ CS Exercises/1 printf.c
@@ -0,0 +1,18 @@
+/* Write a C program displaying a large 11x9 characters 'C' using dashes. The expected output is: */
+
+# include <stdio.h>
+int main (void) {
+
+    printf("   ######\n");
+    printf(" ##      ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##      ##\n");
+    printf("   ######\n");
+
+
+    return 0;
+}
