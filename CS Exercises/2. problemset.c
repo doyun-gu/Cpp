9317677939daeb1e2b9dc356bdcf66095553b2f6
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-20 10:43:00 +0900

    Create 2. problemset.c

---
+++ CS Exercises/2. problemset.c
@@ -0,0 +1,22 @@
+/* The following program is supposed to print a line on the standard output but compilation fails due to several errors:
+
+#include <tdio.h>
+
+void man() {
+    printf("This should work!\n");
+    retur 0;
+}
+
+Correct the program to have it display the following output:
+This should work!
+*/
+
+# include <stdio.h> /* tdio.h -> stdio.h */
+
+void main () {
+
+    printf("This should work!\n");
+
+    /* Void does not have return value */
+
+}
