33a3223ad67a6fb82cf7100399a247a165fafbbf
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-22 11:44:35 +0900

    Update 01-pointer.c

--- CS Exercises/C pointers/01-pointer.c
+++ CS Exercises/C pointers/01-pointer.c
@@ -1,1 +1,46 @@
+/*
+Consider the following program:
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main(int argc, char **argv) {
+    if(argc == 3) {
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+
+        printf("%d + %d = %d\n", a, b, add(a, b));
+    }
+    return 0;
+}
+
+Modify the function add and its invocation so that it takes two int pointer parameters. Examples of output:
+
+./pointer 10 20
+10 + 20 = 30
+
+./pointer 154 -12
+154 + -12 = 142
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int *a, int *b) {
+    return *a + *b;
+}
+
+int main(int argc, char **argv) {
+    if(argc == 3) {
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+
+        printf("%d + %d = %d\n", a, b, add(&a, &b));
+    }
+    
+    return 0;
+}
